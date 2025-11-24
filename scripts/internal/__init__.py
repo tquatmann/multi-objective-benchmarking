7d8072d33934e2df4d3bd57e4b074d0bd405ebdc
Tim Quatmann <tim.quatmann@cs.rwth-aachen.de>
2025-11-24 15:22:22 +0100

    removed unused content

--- scripts/internal/__init__.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-# Catch wrong python version
-if (sys.version_info < (3, 0)):
-    print("ERROR: This script has to be run with python3. Please try the following command-line:\npython3 {}".format(" ".join(sys.argv)))
-    exit(1)
